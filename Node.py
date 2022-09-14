27444ce025ffa9a87c2645bb0cfc072f79cf6ccf
banga87 <angusroymcdonald@gmail.com>
2022-09-14 10:52:27 +1000

    minor edit

--- Node.py
+++ Node.py
@@ -1,7 +1,8 @@
 class Node:
-    def __init__(self, value, next_node=None) -> None:
+    def __init__(self, value, next_node=None, prev_node=None) -> None:
         self.value = value
         self.next_node = next_node
+        self.prev_node = prev_node
 
     def get_value(self):
         return self.value
@@ -11,3 +12,9 @@ class Node:
 
     def set_next_node(self, next_node):
         self.next_node = next_node
+
+    def get_prev_node(self):
+        return self.prev_node
+
+    def set_prev_node(self, prev_node):
+        self.prev_node = prev_node
